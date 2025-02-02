82c4483a725e80bdc5518ca192284e5034116401
alymu <alym7mdd@gmail.com>
2025-02-02 15:18:34 +0200

    Initial commit

---
+++ app/src/main/java/com/example/services/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.services
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val intent = Intent(this, MyServices::class.java)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(intent)
+        }
+        else {
+            startService(intent)
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/services/MyService.kt
@@ -0,0 +1,58 @@
+package com.example.services
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_SHORT_SERVICE
+import android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_SYSTEM_EXEMPTED
+import android.os.Build
+import android.os.Handler
+import android.os.IBinder
+import android.os.Looper
+import android.widget.Toast
+
+class MyServices : Service() {
+    private val serviceId = 1
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "my_channel_1"
+            val channel = NotificationChannel(channelId, "Default", NotificationManager.IMPORTANCE_DEFAULT)
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+            val notification = Notification.Builder(this, channelId).apply {
+                setContentTitle("Notification Title")
+                setContentText("This is content")
+                setSmallIcon(R.drawable.ic_launcher_foreground)
+            }.build()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+                startForeground(serviceId, notification, FOREGROUND_SERVICE_TYPE_SYSTEM_EXEMPTED)
+            }
+        }
+
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        showMessage()
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun showMessage() {
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                Toast.makeText(applicationContext, "HELLO!", Toast.LENGTH_SHORT).show()
+            },
+            5000
+        )
+    }
+}
